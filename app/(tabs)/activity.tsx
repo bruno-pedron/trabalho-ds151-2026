398041d58ee8eadf7de03f40fc984c6d47cf1fc5
bruno-pedron <brunorupaner09@gmail.com>
2026-05-16 22:27:53 -0300

    issue #23: criação das tabs que serão usadas no app

---
+++ app/(tabs)/activity.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function ActivityScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Atividade / Extrato</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -16,6 +16,8 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  'doc.text.fill': 'description',
+  'person.circle': 'account-circle',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
 } as IconMapping;
