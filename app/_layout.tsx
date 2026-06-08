4c76ef53458446724528588e282555bd37272785
Kaironst <pedronovak4@gmail.com>
2026-06-08 19:51:44 -0300

    feat: feita implementação da resposta de redefinir senha

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,12 +9,21 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { useAuth } from '@/hooks/useAuth';
 import { useEffect } from 'react';
 import { ActivityIndicator, Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import * as Linking from 'expo-linking'
+import { supabase } from '@/supabase/supabase';
 
 function RootLayoutNav() {
   const { session, isLoading } = useAuth();
   const segments = useSegments();
   const router = useRouter();
   const colorScheme = useColorScheme();
+  const url = Linking.useURL();
+
+  useEffect(() => {
+    if (url) {
+      handleLink(url);
+    }
+  }, [url]);
 
   useEffect(() => {
 
@@ -29,6 +38,34 @@ function RootLayoutNav() {
       router.replace('/(tabs)/profile')
     }
   }, [session, isLoading, segments]);
+
+  async function handleLink(url: string) {
+    const { queryParams } = Linking.parse(url)
+
+    if (queryParams?.error_description) {
+      console.error("Erro no link de redefinir senha", queryParams.error_description);
+      return;
+    }
+
+    const parts = url.split('#');
+    if (parts.length > 1) {
+      const hash = parts[1]
+      const params = Object.fromEntries(new URLSearchParams(hash).entries())
+
+      const accessToken = params.access_token;
+      const refreshToken = params.refresh_token;
+
+      if (accessToken && refreshToken) {
+        const { error } = await supabase.auth.setSession({
+          access_token: accessToken,
+          refresh_token: refreshToken,
+        });
+        if (!error) router.replace('/(tabs)/profile');
+      }
+
+
+    }
+  }
 
   if (isLoading) return <ActivityIndicator />
 
