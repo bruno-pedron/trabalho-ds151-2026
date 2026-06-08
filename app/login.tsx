57e5637f8a3529801133d3dbac050162beea8da7
Kaironst <pedronovak4@gmail.com>
2026-06-08 17:36:39 -0300

    feat: tela inicial de login preparada para ciração do redefinir senha

--- app/login.tsx
+++ app/login.tsx
@@ -3,7 +3,7 @@ import { ThemedView } from '@/components/themed-view';
 import { useThemeColor } from '@/hooks/use-theme-color';
 import { router } from 'expo-router';
 import { useState } from 'react';
-import { Alert, Pressable, StyleSheet, Text, TextInput } from 'react-native';
+import { Alert, Modal, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { supabase } from '@/supabase/supabase';
 
 export default function Login() {
@@ -13,6 +13,9 @@ export default function Login() {
 
   const backgroundColor = useThemeColor({}, 'background');
   const textColor = useThemeColor({}, "text")
+
+  const [editModalVisible, setEditModalVisible] = useState(false)
+  const [inputValue, setInputValue] = useState("");
 
   const submit = async () => {
     const { error } = await supabase.auth.signInWithPassword({
@@ -33,12 +36,39 @@ export default function Login() {
     router.push("/cadastro")
   };
 
+
+  const submitEdit = async () => {
+    console.log("redefinir senha called");
+    try {
+      //não funciona no expo
+      const { data, error } = await supabase.auth.resetPasswordForEmail(inputValue, {
+        /*redirectTo depende do ip do computador rodando expo, ou utiliza uma key padrão definida em app.json como shcema em produção*/
+      });
+      //error ? 
+      Alert.alert("falha na alteração")
+      //: Alert.alert("senha alterada");
+    }
+    catch (err) {
+      Alert.alert("falha na alteração")
+    }
+  }
+
   const styles = StyleSheet.create({
     titulo: {
       fontSize: 26,
       lineHeight: 30,
       marginBottom: "5%",
       marginTop: "20%"
+    },
+    container: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: backgroundColor
+    },
+    content: {
+      alignItems: 'center',
+      gap: 16,
+      paddingTop: 40,
     },
     input: {
       color: textColor,
@@ -52,13 +82,16 @@ export default function Login() {
     button: {
       borderColor: "#2020aa",
       backgroundColor: "#8099ff",
-      borderWidth: 2,
       borderRadius: 5,
-      width: "30%",
-      height: "5%",
+      borderWidth: 2,
+      paddingHorizontal: 16,
+      paddingVertical: 8,
+    },
+    recoverPassword: {
+      color: "#606060",
       alignItems: "center",
-      justifyContent: "center"
-
+      justifyContent: "center",
+      fontSize: 10,
     }
 
   });
@@ -70,6 +103,44 @@ export default function Login() {
       alignItems: 'center',
       gap: 10,
     }}>
+
+      <Modal
+        animationType="slide"
+        visible={editModalVisible}
+        onRequestClose={() => {
+          setEditModalVisible(!editModalVisible);
+        }}>
+        <ThemedView style={styles.container}>
+          <View style={styles.content}>
+            <ThemedText style={styles.titulo} >Redefinindo senha</ThemedText>
+
+            <TextInput
+              onChangeText={setInputValue}
+              placeholder={"digite o seu email para recuperação"}
+              placeholderTextColor={textColor}
+              style={[styles.input, { color: textColor }]}
+              value={inputValue}
+            />
+
+            <Pressable
+              style={styles.button}
+              onPress={() => setEditModalVisible(!editModalVisible)}>
+              <ThemedText>Cancelar</ThemedText>
+            </Pressable>
+
+            <Pressable
+              style={styles.button}
+              onPress={async () => {
+                await submitEdit();
+                setEditModalVisible(!editModalVisible)
+              }}>
+              <ThemedText>Confirmar</ThemedText>
+            </Pressable>
+
+          </View>
+        </ThemedView>
+      </Modal>
+
       <ThemedText style={styles.titulo}>faça login para continuar</ThemedText>
       <TextInput
         autoCapitalize='none'
@@ -88,6 +159,11 @@ export default function Login() {
         style={styles.input}
         value={senha}
       />
+
+      <Pressable onPress={() => { setEditModalVisible(!editModalVisible) }}>
+        <Text style={styles.recoverPassword}> esqueci a senha {'\n'} </Text>
+      </Pressable>
+
       <Pressable onPress={submit} style={styles.button}>
         <Text style={{ color: textColor }}> ENVIAR </Text>
       </Pressable>
@@ -98,8 +174,3 @@ export default function Login() {
     </ThemedView>
   );
 };
-
-
-
-
-
